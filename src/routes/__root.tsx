bc37c1262ca42b492ac0602b9363ac20debca896
khaibq <quangkhai9898@gmail.com>
2025-10-31 08:03:25 +0700

    remove unused import

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,4 @@
 import { Link, Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanStackDevtools } from '@tanstack/react-devtools'
 
 export const Route = createRootRoute({
   component: () => (
